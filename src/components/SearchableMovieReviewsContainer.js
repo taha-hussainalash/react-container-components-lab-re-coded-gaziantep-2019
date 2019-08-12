92640709a2e6c4f9aa80864f73003e5b5dc9a3bf
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-12 23:25:06 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,6 +7,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
 class SearchableMovieReviewsContainer extends Component {
   state = {
     searchTerm: '',
