e6f2e60c1030f5f6e27c10a9dcf07f43b338f428
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-12 23:31:39 +0000

    Automatically backed up by Learn

--- test/SearchableMovieReviewsContainer_test.js
+++ test/SearchableMovieReviewsContainer_test.js
@@ -8,7 +8,6 @@ import testReviews from './test-reviews';
 import { spy, stub, useFakeTimers } from 'sinon'
 
 Enzyme.configure({ adapter: new Adapter() })
-
 const Noop = (props) => { return <p>Noop</p> };
 
 describe('<SearchableMovieReviewsContainer />', () => {
